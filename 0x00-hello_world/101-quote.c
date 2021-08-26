57f67cb51a2e7b5cc277a98cc4a8f80f4000a60d
xtrahuman <okpara_tochukwu@yahoo.com>
2021-08-26 13:07:42 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-int st = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int st = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\
+");
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", st);
 return (1);
 }
