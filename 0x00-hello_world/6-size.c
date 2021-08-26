3109e7e10e3f9b8419b7491c1bc9f7f14939877d
xtrahuman <okpara_tochukwu@yahoo.com>
2021-08-26 12:17:28 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,8 @@ int main(void)
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long lo\
+ng int));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
