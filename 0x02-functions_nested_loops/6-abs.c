1a293423047aef0d3d95f85449d68780c15ef344
xtrahuman <okpara_tochukwu@yahoo.com>
2021-08-31 13:56:27 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - converts negative numbers to positive
+ * @n: The character to print
+ *
+ * Return: -n if negative else it returns n.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
