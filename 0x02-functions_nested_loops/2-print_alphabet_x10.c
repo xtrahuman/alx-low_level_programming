fcb1bebcb5f846cf88bdd113f438f98cb8364df6
xtrahuman <okpara_tochukwu@yahoo.com>
2021-08-31 07:19:59 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet 10 times
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+void print_alphabet(void)
+{
+	char i;
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+
+}
