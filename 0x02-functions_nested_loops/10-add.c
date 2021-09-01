7806a6ddf9517358b3ed7f93fc913c6ba52e8086
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-01 01:34:21 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i: first parameter
+ * @j: second parameter
+ * Return: result of addition
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
