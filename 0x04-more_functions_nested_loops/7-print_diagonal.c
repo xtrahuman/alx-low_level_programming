b6b305f16651148613a6cc879962d44353e5b10d
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-02 12:47:57 -0700

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,23 +6,22 @@
 
 void print_diagonal(int n)
 {
-	int c, i, d;
+	int c, i;
 
 	c = 0;
 
 	while (n > 0)
 	{
 		i = c;
-
 		while (i > 0)
 		{
-			_putchar(32);
+			_putchar(' ');
 			i--;
 		}
 		_putchar('\\');
 		_putchar('\n');
-		n--;
 		c++;
+		n--;
 	}
 	if (c < 1)
 		_putchar('\n');
