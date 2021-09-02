8dd73068d3d6ca541845fe14fbb0689d81814be2
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-02 05:10:45 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
@@ -10,6 +11,7 @@
 */
 
 void print_remaining_days(int month, int day, int year)
+
 {
 	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
 	{
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
