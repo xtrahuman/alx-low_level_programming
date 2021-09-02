efd2d7aff61e75291ba7697a24c37fa6dcb27af4
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-02 05:15:44 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,11 +7,9 @@
 * @month: month in number format
 * @day: day of month
 * @year: year
-* Return: void
 */
 
 void print_remaining_days(int month, int day, int year)
-
 {
 	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
 	{
