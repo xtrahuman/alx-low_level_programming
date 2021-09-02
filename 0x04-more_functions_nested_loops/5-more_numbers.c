8ce88fcc6fc931352b72f8cfe0b3852451637745
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-02 12:31:17 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,12 @@ void more_numbers(void)
 		{
 
 			if (i > 9)
+			{
 
-				j = i / 10;
-				k = i % 10;
-				_putchar(j + '0');
-				_putchar(k + '0');
+				_putchar(i / 10 + '0')
+			}
+
+				_putchar(i % 10 + '0');
 
 		}
 
