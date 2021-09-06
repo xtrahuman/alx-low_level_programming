b07fbdc50e2ee97a857c7e4e7d4d59c7d7328fc4
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-06 05:52:07 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - takes a parameter and updates the value to 98
- *
+ * @n: integer to be updated
  * Return: 0
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
 
-	return (0);
 }
