dd4ef06f0efe587b457eb50069781c16f6c8c8b9
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-06 13:49:13 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
