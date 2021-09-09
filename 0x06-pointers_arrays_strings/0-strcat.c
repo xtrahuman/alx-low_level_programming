bf340327fee9e5264b014e047eb9d6b47f2f42b5
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-09 04:55:36 -0700

    update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concantenates two stings
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] !='\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,10 @@
+# more on pointers arrays and strings
+
+## major topics covered
+
+* strcat
+* strncat
+* strncpy
+* strcpy
+* strcmp
+* reverseArray
