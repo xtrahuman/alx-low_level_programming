ee2129619939d2a992acb1121fec1e3a0abc1c99
xtrahuman <okpara_tochukwu@yahoo.com>
2021-09-09 20:45:59 -0700

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
+		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
+		else if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
+		else if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
+		else if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
+		else if (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 	}
 
